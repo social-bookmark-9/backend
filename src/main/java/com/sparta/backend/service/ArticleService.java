20dec153062321eab7658a558666ffd69a6b36e3
Lim Hyunwoo <hyunwoome@icloud.com>
2022-03-16 02:47:50 +0900

    fix: Git Error 해결

--- src/main/java/com/sparta/backend/service/ArticleService.java
+++ src/main/java/com/sparta/backend/service/ArticleService.java
@@ -1,16 +1,16 @@
 package com.sparta.backend.service;
 
+import com.sparta.backend.model.Member;
 import com.sparta.backend.requestDto.ArticleCreateRequestDto;
 import com.sparta.backend.requestDto.ArticleReviewRequestDto;
 import com.sparta.backend.requestDto.ArticleUpdateRequestDto;
 import com.sparta.backend.responseDto.ArticleResponseDto;
-import com.sparta.backend.responseDto.ArticleReviewHideResponseDto;
 import com.sparta.backend.responseDto.ArticleReviewResponseDto;
 
 public interface ArticleService {
-    ArticleResponseDto getArticle(long id);
-    long createArticle(ArticleCreateRequestDto requestDto);
-    void updateArticle(ArticleUpdateRequestDto requestDto, long id);
-    ArticleReviewResponseDto updateArticleReview(ArticleReviewRequestDto requestDto, long id);
-    ArticleReviewHideResponseDto updateArticleReviewHide(long id);
+    ArticleResponseDto getArticle(long id, Member member);
+    long createArticle(ArticleCreateRequestDto requestDto, Member member);
+    void updateArticle(ArticleUpdateRequestDto requestDto, long id, Member member);
+    ArticleReviewResponseDto updateArticleReview(ArticleReviewRequestDto requestDto, long id, Member member);
+    boolean updateArticleReviewHide(long id);
 }
