e2c267b41f91ec8217833b7954f1b074e4e28a4a
Lim Hyunwoo <hyunwoome@icloud.com>
2022-03-18 14:27:21 +0900

    feat: 아티클 상세페이지에서 '함께보면 좋은 글'기능 생성 외 리팩토링

---
+++ src/main/java/com/sparta/backend/responseDto/ArticleGetResponseDto.java
@@ -0,0 +1,25 @@
+package com.sparta.backend.responseDto;
+
+import com.sparta.backend.model.Article;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ArticleGetResponseDto {
+    private final Long articleId;
+    private final String url;
+    private final String titleOg;
+    private final String imgOg;
+    private final String contentOg;
+    private final String hashtag1;
+    private final String hashtag2;
+    private final String hashtag3;
+    private final String review;
+    private final Boolean reviewHide;
+    private final Integer reminderDate;
+    private final String articleFolderName;
+    private final List<ArticleRandomResponseDto> recommendArticles;
+}
