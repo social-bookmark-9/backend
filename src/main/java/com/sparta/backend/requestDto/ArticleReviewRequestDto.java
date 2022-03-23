bfb88d7d31330f2be45aa122e938a0bab5d637d6
Lim Hyunwoo <hyunwoome@icloud.com>
2022-03-23 11:08:55 +0900

    feat: 리뷰 리스트 추가

--- src/main/java/com/sparta/backend/requestDto/ArticleReviewRequestDto.java
+++ src/main/java/com/sparta/backend/requestDto/ArticleReviewRequestDto.java
@@ -1,13 +1,17 @@
 package com.sparta.backend.requestDto;
 
 import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ArticleReviewRequestDto {
     @NotNull
-    private final String review;
+    private String review;
 }
