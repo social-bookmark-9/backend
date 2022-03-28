7e2e333cb09cf0343856f61c98eb88f1b5436382
wowba <leeyuwk54@gmail.com>
2022-03-28 13:59:21 +0900

    refactor: 프론트 연결용 redirect uri 추가

--- src/main/java/com/sparta/backend/serviceImpl/OauthServiceImpl.java
+++ src/main/java/com/sparta/backend/serviceImpl/OauthServiceImpl.java
@@ -63,9 +63,10 @@ public class OauthServiceImpl implements OauthService {
         body.add("grant_type", "authorization_code");
         body.add("client_id", clientId);
 
-//        body.add("redirect_uri", "http://localhost:3000/api/users/login"); // 프론트 로컬 연결 테스트용
-        body.add("redirect_uri", "http://finalproject9.s3-website.ap-northeast-2.amazonaws.com/api/users/login"); // 프론트 서버 연결 테스트용
+        body.add("redirect_uri", "http://localhost:3000/api/users/login"); // 프론트 로컬 연결 테스트용
+//        body.add("redirect_uri", "http://finalproject9.s3-website.ap-northeast-2.amazonaws.com/api/users/login"); // 프론트 서버 연결 테스트용
 //        body.add("redirect_uri", "http://3.34.99.169/api/users/login"); // 서버 연결 테스트용
+//        body.add("redirect_uri", "http://localhost:8080/api/users/login"); // 서버 연결 테스트용
         body.add("code", code);
 
         // HTTP 요청 보내기
