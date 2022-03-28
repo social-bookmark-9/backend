56c00fa96250f2e0ec20f398f836c9cb1bebf6c2
wowba <leeyuwk54@gmail.com>
2022-03-28 21:18:57 +0900

    refactor: 테스트 yml 수정

--- src/test/java/com/sparta/backend/BackendApplicationTests.java
+++ src/test/java/com/sparta/backend/BackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" + "classpath:/application.yml" )
 class BackendApplicationTests {
 
     @Test
