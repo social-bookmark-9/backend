cda72c0d9e30a0c6bdf9f664fc93b39420182f75
wowba <leeyuwk54@gmail.com>
2022-03-30 15:33:39 +0900

    refactor: 이메일 메세지 수정 및 리프레시토큰 제거 수정

---
+++ src/test/java/com/sparta/backend/serviceImpl/ReminderServiceTest.java
@@ -0,0 +1,32 @@
+package com.sparta.backend.serviceImpl;
+
+import com.sparta.backend.repository.ReminderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReminderServiceTest {
+
+//    @Mock
+//    ReminderRepository reminderRepository;
+//
+//    @Test
+//    void getReminders() {
+//    }
+//
+//    @Test
+//    void editReminder() {
+//    }
+//
+//    @Test
+//    void createReminder() {
+//    }
+//
+//    @Test
+//    void deleteReminder() {
+//    }
+}
