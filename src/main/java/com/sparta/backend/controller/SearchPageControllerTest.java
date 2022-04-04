80f9c900a4d405b96c3f194138e21818a7ed3d24
wowba <leeyuwk54@gmail.com>
2022-04-04 15:02:36 +0900

    refactor : 메인페이지 아티클 추천 및 검색페이지 param 디코딩 방식 수정

--- src/main/java/com/sparta/backend/controller/SearchPageControllerTest.java
+++ src/main/java/com/sparta/backend/controller/SearchPageControllerTest.java
@@ -11,9 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.bind.DatatypeConverter;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Map;
 
 @RestController
@@ -24,17 +23,15 @@ public class SearchPageControllerTest {
 
     // 아티클 해시태그 검색 최신순으로 가져오기
     @GetMapping("/api/searchpage/articles")
-    public ResponseEntity<RestResponseMessage> getArticles(@RequestParam String hashtag, @RequestParam String titleOg, @RequestParam int page, @RequestParam String sort) {
+    public ResponseEntity<RestResponseMessage> getArticles(@RequestParam String hashtag, @RequestParam String titleOg, @RequestParam int page, @RequestParam String sort) throws UnsupportedEncodingException {
 
         PageRequest pageRequest = PageRequest.of(page, 6, Sort.by(Sort.Direction.DESC, sort));
 
-        // 해쉬태그 디코드
-        byte[] hashtagDecoded = Base64.getDecoder().decode(hashtag);
-        hashtag = new String(hashtagDecoded, StandardCharsets.UTF_8);
+        // titleOg 디코딩
+        titleOg = URLDecoder.decode(titleOg, "UTF-8");
 
-        // 검색어 디코드
-        byte[] titleOgDecoded = Base64.getDecoder().decode(titleOg);
-        titleOg = new String(titleOgDecoded, StandardCharsets.UTF_8);
+        // hashtag 디코딩
+        hashtag = URLDecoder.decode(hashtag, "UTF-8");
 
         Map<String, Object> articleList = searchPageServiceTest.getSearchArticles(hashtag, titleOg, pageRequest);
 
@@ -43,17 +40,15 @@ public class SearchPageControllerTest {
     
     // 아티클 폴더 해시태그 검색 최신순, 좋아요 순
     @GetMapping("/api/searchpage/articlefolders")
-    public ResponseEntity<RestResponseMessage> getArticleFolders(@RequestParam String hashtag, @RequestParam String titleOg, @RequestParam int page, @RequestParam String sort) {
+    public ResponseEntity<RestResponseMessage> getArticleFolders(@RequestParam String hashtag, @RequestParam String titleOg, @RequestParam int page, @RequestParam String sort) throws UnsupportedEncodingException {
 
         PageRequest pageRequest = PageRequest.of(page, 6, Sort.by(Sort.Direction.DESC, sort));
 
-        // 해쉬태그 디코드
-        byte[] hashtagDecoded = Base64.getDecoder().decode(hashtag);
-        hashtag = new String(hashtagDecoded, StandardCharsets.UTF_8);
+        // titleOg 디코딩
+        titleOg = URLDecoder.decode(titleOg, "UTF-8");
 
-        // 검색어 디코드
-        byte[] titleOgDecoded = Base64.getDecoder().decode(titleOg);
-        titleOg = new String(titleOgDecoded, StandardCharsets.UTF_8);
+        // hashtag 디코딩
+        hashtag = URLDecoder.decode(hashtag, "UTF-8");
 
         Map<String, Object> articleFolderList = searchPageServiceTest.getSearchArticleFolders(hashtag, titleOg, pageRequest);
 
