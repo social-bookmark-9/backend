4c6bfa6272ad7d6ef3d3f9f01c951e560e303dfa
hyunwoome <hyunwoome@icloud.com>
2022-04-06 23:02:06 +0900

    hotfix: Collector import  실패 해결

--- src/main/java/com/sparta/backend/serviceImpl/MainPageServiceImpl.java
+++ src/main/java/com/sparta/backend/serviceImpl/MainPageServiceImpl.java
@@ -24,7 +24,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
